f40c1bca25f88a2dfb69fcd9e7c8ce36349b1c21
Nazli Ander <mail@nander.cc>
2020-10-25 14:44:02 +0100

    Create a POST request method for retrieving book word counts
    Book URLs can be sent to the API, the response will be a JSON
    containing word counts for the book URL.
    
    Signed-off-by: Nazli Ander <mail@nander.cc>

--- app/bookAnalytics.go
+++ app/bookAnalytics.go
@@ -6,22 +6,50 @@ import (
 	"net/http"
 )
 
-var (
-	// Sherlock book link
-	searchString string = "https://www.gutenberg.org/files/1661/1661-0.txt"
-	// Path to store Sherlock Book
-	pathToStoreBook string = "./book.txt"
-)
+type BookData struct {
+	URL        string         `json:"URL"`
+	WordCounts map[string]int `json:"BookWordCounts"`
+}
+
+type BookRequest struct {
+	URL string `json:"URL"`
+}
+
+func bookAnalytics(w http.ResponseWriter, r *http.Request) {
+
+	var br BookRequest
+
+	switch r.Method {
+
+	case "POST":
+
+		err := json.NewDecoder(r.Body).Decode(&br)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		bookURL := br.URL
+
+		bookText := collectBook(bookURL)
+		bookWordCounts := wordCount(bookText)
+
+		bookData := BookData{
+			URL:        bookURL,
+			WordCounts: bookWordCounts,
+		}
+
+		bookDataResponse, _ := json.Marshal(bookData)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bookDataResponse)
+
+	default:
+		fmt.Fprintf(w, "Only POST method is supported.")
+	}
+}
 
 func main() {
-	http.HandleFunc("/book", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Please visit: http://localhost:7979/book")
-		bookText := collectBook(searchString)
-		bookWordCounts := wordCount(bookText)
-		jsonCounts, _ := json.Marshal(bookWordCounts)
-		fmt.Fprintln(w, string(jsonCounts))
-		storeBookText(bookText, pathToStoreBook)
-	})
-
+	http.HandleFunc("/book-collect", bookAnalytics)
 	http.ListenAndServe(":7979", nil)
 }
